1568fd1f8a350b89607d0c1e2624c88850342d71
stajun <stajun@naver.com>
2020-04-20 14:13:58 +0900

    2일차-if문홀짝예제

---
+++ src/day2/IfEx1.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		// if문 예제1
+		int num = 4;
+		if(num % 2 == 0 ){
+			System.out.println(num+"는 짝수");
+		}
+		if(num % 2 == 1 ){ //if(num % 2 != 0 ){
+			System.out.println(num+"는 홀수");
+		}
+		//if else문 예제
+		if(num % 2 == 0 ){
+			System.out.println(num+"는 짝수");
+		}else{ 
+			System.out.println(num+"는 홀수");
+		}
+		//if else if문 예제
+		if(num % 2 == 0 ){
+			System.out.println(num+"는 짝수");
+		}else if(num % 2 == 1){ 
+			System.out.println(num+"는 홀수");
+		}
+	}
+
+}
