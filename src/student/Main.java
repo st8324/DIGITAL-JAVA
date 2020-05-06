4faaca1a13e9822f200595f19a284ceedbc5692d
stajun <stajun@naver.com>
2020-05-06 17:54:27 +0900

    10일차-학생관리프로그램1차

---
+++ src/student/Main.java
@@ -0,0 +1,28 @@
+package student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setScore(new Subject("국어",40,40,10,10),
+				new Subject("영어",30,10,10,10),
+				new Subject("수학",20,20,10,10));
+		s.printScore();
+	}
+}
+/* 1. 데이터 관리를 위한 작업
+ *   => 변수나 클래스를 이용
+ *   학생 정보=>Student 클래스
+ *   과목 정보=>Subject 클래스
+ * 2. 기능 구현
+ *   => main메소드에 바로 구현
+ * 	 => 또는 기능을 인터페이스로 정의하고, 해당 인터페이스를 구현하는 구현 클래스
+ * 3. 테스트
+ * */
+interface Manage{
+	public int insertStuent(Student []arr, Student std);
+	public void modifyStuent(Student []arr, Student std);
+	public int deleteStuent(Student []arr, Student std);
+	public void printStuent(Student []arr,int count);
+}
+
---
+++ src/student/Student.java
@@ -0,0 +1,57 @@
+package student;
+
+public class Student {
+	//멤버 변수 설정
+	private String name="";
+	private int grade;
+	private int classNum;
+	private int num;
+	private Subject score[];
+	
+	//기능 설정
+	public boolean equal(int grade, int classNum, int num, String name) {
+		if(this.grade != grade)	return false;
+		if(this.classNum != classNum) return false;
+		if(this.num != num) return false;
+		if(!this.name.equals(name))	return false;
+		return true;
+	}
+	
+	//getter, setter 설정
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getClassNum() {
+		return classNum;
+	}
+	public void setClassNum(int classNum) {
+		this.classNum = classNum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public void setScore(Subject...score) {
+		this.score = new Subject[score.length];
+		for(int i = 0 ; i<score.length; i++) {
+			//this.score[i] = score[i];
+			this.score[i] = new Subject(score[i]);
+		}
+	}
+	public void printScore() {
+		for(Subject tmp : score) {
+			tmp.print();
+		}
+	}
+}
