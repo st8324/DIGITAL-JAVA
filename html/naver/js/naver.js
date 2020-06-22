2949563037636bbf8b1e9da187bf2386a00f7ffd
stajun <stajun@naver.com>
2020-06-22 11:06:57 +0900

    네이버 화면 구현하기4

--- html/naver/js/naver.js
+++ html/naver/js/naver.js
@@ -16,10 +16,32 @@ $(function(){
 		$('.btn-auto-search').click();
 		e.preventDefault();
 	})
+	//더보기/접기 버튼 클릭
 	$('.btn-more').click(function(){
 		$('.box-group-keyword').toggleClass('display-none');
-		$('.box-service-menu').toggleClass('display-none');
+		$('.box-service-menu.display').toggleClass('display-none');
 		$('.box-container-servicelist').toggleClass('display-none');
 		$('.btn-more').toggleClass('display-none');
+		if($(this).hasClass('fold')){
+			initMenu();
+		}
 	})
+	$('.box-serivce-check').click(function(){
+		$(this).find('.icon-check').toggleClass('checked');
+	})
+	//메뉴 설정 버튼 클릭
+	$('.btn-menu-set').click(function(){
+		$('.box-service-menu.display').addClass('display-none');
+		$('.box-service-menu.set').removeClass('display-none');
+		$('.box-container-servicelist .container.display').addClass('display-none');
+		$('.box-container-servicelist .container.set').removeClass('display-none');
+	})
+	function initMenu(){
+		$('.box-service-menu.display').addClass('display-none');
+		$('.box-service-menu.set').addClass('display-none');
+		$('.box-container-servicelist .container.display').removeClass('display-none');
+		$('.box-container-servicelist .container.set').addClass('display-none');
+		//메뉴 설정에서 선택된 체크박스를 전부 해제
+		$('.icon-check').removeClass('checked');
+	}
 })
